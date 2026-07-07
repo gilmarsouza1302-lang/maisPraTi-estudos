98cca1bc33c2493efb30e9f0020b61c8f1b95afa
gilmar.souza1302-lang <gilmar.souza1302@gmail.com>
2026-07-07 19:11:24 -0300

    Atualiza estudos de Frameworks CSS e Promises

---
+++ Module02 - Frontend/sample-project/src/App.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+ function saudacao( {nome}){
+  return <h2>"Olá, " { nome } " Seja bem-vindo (a)!</h2>;
+ }
+ export default function App() {
+  
+  const [contador, setContador] = useState(0);
+
+  return (
+         <div>
+          <saudacao nome="João" />
+          <p>
+             {contador}
+          </p>
+          <button onClick={() => setContador(contador + 1)}>Incrementar Soma +1
+
+          </button>
+
+          <button onClick={() => setContador(contador - 1)}>Decrementar Subtração -1</button>
+         </div>
+    
+  );
+
+ 
+ }
