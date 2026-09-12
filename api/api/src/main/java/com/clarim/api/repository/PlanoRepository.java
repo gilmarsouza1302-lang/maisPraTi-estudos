0b987aa18be57bc194f05202e6ac24e0acea9ac5
jhyago <jaquesantunes@protonmail.com>
2026-09-12 11:26:08 -0300

    Add plan, tag and subscription repositories

---
+++ api/api/src/main/java/com/clarim/api/repository/PlanoRepository.java
@@ -0,0 +1,11 @@
+package com.clarim.api.repository;
+
+import com.clarim.api.model.Plano;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PlanoRepository extends JpaRepository<Plano, Long> {
+    Optional<Plano> findByAtivoTrue();
+    Optional<Plano> findByStripePriceId(String stripePriceId);
+}
